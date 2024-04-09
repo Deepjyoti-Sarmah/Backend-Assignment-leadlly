534175f644572ada0e50fb6f0986c902072e9618
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-04-09 20:37:30 +0530

    adding dockerfile and docker-compose file

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const DB_NAME = "leadlly";
+export const MONGODB_URI=`mongodb://${process.env.DB_ADMIN}:${process.env.DB_ADMIN_PWD}@localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { MONGODB_URI } from "../constants";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    const connectionInstance = await mongoose.connect(`${MONGODB_URI}`);
     console.log(`\n MongoDB connected || DB HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error", error);
-    process.exit(1)
+    process.exit(1);
   }
 }
 
