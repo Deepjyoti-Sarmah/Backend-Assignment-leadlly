f196674d8e8a83dd6dd1e55990856bbfde9a4344
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-04-11 14:50:28 +0530

    added docker file for the project and connected backend with mongo image using docker

--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-const {
-  DB_ADMIN, DB_PASSWORD, DB_PORT, DB_NAME
-} = process.env;
-
-export const MONGODB_URI = `mongodb://${DB_ADMIN}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}?authSource=admin`
-
